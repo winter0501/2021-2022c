18db07c3257b84bc95349f5659fecc74a6904632
winter0501 <rick925119@gmail.com>
2021-12-02 17:45:32 +0800

    加入week03

--- week03-3.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("Hello World\n");
-    printf("Hello%dWorld\n",300);
-    printf("%d %d %d\n",100,200,300);
-
-}
-
---
+++ week03/week03-3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello%dWorld\n",300);
+    printf("%d %d %d\n",100,200,300);
+
+}
+
